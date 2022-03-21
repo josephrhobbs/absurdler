1f77a028d2b4ed5121a8f81f40707d8da6695d1b
hobbsbros <89106819+hobbsbros@users.noreply.github.com>
2022-03-21 18:49:10 -0400

    Wrote Python script to parse words from Wordle source code

---
+++ src/words/words.py
@@ -0,0 +1,16 @@
+# words.py
+#
+# Parses raw words list from Wordle source code
+
+raw = open("raw.txt", "r").read()
+words = open("words.txt", "w+")
+
+rawlist = raw.split(",")
+
+wordlist = [w.strip("[]\" ") for w in rawlist]
+
+for w in wordlist:
+    words.write(w)
+    words.write("\n")
+
+words.close()
