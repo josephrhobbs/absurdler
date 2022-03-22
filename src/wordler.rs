682646b6175c46df87c43d20a7653b1804a523b9
hobbsbros <89106819+hobbsbros@users.noreply.github.com>
2022-03-22 14:01:06 -0400

    Allowed for user choice of guess

--- src/wordler.rs
+++ src/wordler.rs
@@ -26,11 +26,20 @@ fn main() {
             println!("I guess {}\n", top_guess.to_string().to_uppercase());
         }
 
+        // Get human's guess
+        print!("What do you guess? >> ");
+        io::stdout().flush().unwrap();
+
+        let mut human_guess: String = String::new();
+        io::stdin().read_line(&mut human_guess).expect("Could not read input");
+        human_guess = human_guess.to_lowercase();
+
         // Get Wordle's response from the user
-        print!("How did I do? >> ");
+        print!("How did you do? >> ");
         io::stdout().flush().unwrap();
         combination = String::new();
         io::stdin().read_line(&mut combination).expect("Could not read line from stdin");
+        println!("\n");
 
         combination.pop();  combination.pop();
 
@@ -39,7 +48,7 @@ fn main() {
             break;
         }
 
-        words = words::get_matches(top_guess.to_string().clone(), words.clone(), combination.clone());
+        words = words::get_matches(human_guess.to_string().clone(), words.clone(), combination.clone());
     }
     println!("\nThanks for playing!\n");
 }
